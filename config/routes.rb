44d23c5cbe8f9fc9248a44d574cc79374a884b58
Nahum <losond8@gmail.com>
2021-07-21 19:11:40 -0700

    Understanding RESTful routing conventions in Rails, use resources to generate routes, understand how to view all routes in a Rails application.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # #show a list of all birds
+  # get '/birds', to: 'birds#index'
+
+  # #show an individual bird
+  # get '/birds/:id', to: 'birds#show'
+
+  resources :birds, only: [:index, :show]
 end
+
+
